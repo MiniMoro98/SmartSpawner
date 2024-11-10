31dee5779dc9f9e9d4d6365de9b3c9d3336a4594
MiniMoro98 <moro18391@gmail.com>
2024-11-10 16:31:00 +0100

    Primo caricamento

---
+++ src/main/java/it/moro/smartspawner/SmartSpawner.java
@@ -0,0 +1,17 @@
+package it.moro.smartspawner;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SmartSpawner extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
